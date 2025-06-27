8652d1a25cad3106762e99e5461c1744392b0279
Leo Howell <162366629+leohowell18@users.noreply.github.com>
2025-06-26 21:29:44 -0400

    move-type-lookup

---
+++ move-type-lookup.js
@@ -0,0 +1,12 @@
+{
+  "earthquake": "ground",
+  "dragon-claw": "dragon",
+  "play-rough": "fairy",
+  "belly-drum": "normal",
+  "drain-punch": "fighting",
+  "rage-fist": "ghost",
+  "shadow-ball": "ghost",
+  "iron-head": "steel",
+  "brave-bird": "flying",
+  "recover": "normal"
+}
